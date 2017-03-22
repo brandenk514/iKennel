718a3a8be2accc3c4d9a2e8773a54694b14ff606
Branden <brandenk514@gmail.com>
2017-03-21 21:45:22 -0400

    Began work on adding another animal to a current client

--- iKennel/EditAnimalViewController.swift
+++ iKennel/EditAnimalViewController.swift
@@ -37,7 +37,8 @@ class EditAnimalViewController: UIViewController {
         dateInButton.setTitle(sel_animal.getDMY_time(d: sel_animal.getReservation().dateIn), for: .normal)
         dateOutButton.setTitle(sel_animal.getDMY_time(d: sel_animal.getReservation().dateOut), for: .normal)
         checkedInSwitch.isOn = sel_animal.getReservation().checkedIn
-
+        
+        //print(sel_animal)
 
         // Do any additional setup after loading the view.
     }
