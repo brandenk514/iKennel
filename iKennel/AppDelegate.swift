d259c5f678136062a34a3618bbc7cbf86250e744
Branden <brandenk514@gmail.com>
2017-03-29 17:13:08 -0400

    Updated Color scheme

--- iKennel/AppDelegate.swift
+++ iKennel/AppDelegate.swift
@@ -13,9 +13,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let navigationBarAppearace = UINavigationBar.appearance()
+        
+        navigationBarAppearace.tintColor = UIColor.white
+        navigationBarAppearace.barTintColor = UIColor.init(red: 255.0/255.0, green: 70.0/255.0, blue: 51.0/255.0, alpha: 1.0)
+        navigationBarAppearace.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
         return true
     }
 
